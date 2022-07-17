cd0809a47bd1d6736ff69bc85f05d61c002b64ce
raulescobar-g <raulescobar_g@tamu.edu>
2022-07-17 12:45:15 -0500

    fixed build

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
 import adapter from '@sveltejs/adapter-auto';
+import { optimizeImports } from "carbon-preprocess-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter()
-	}
+	},
+	preprocess: [optimizeImports()]
 };
 
 export default config;
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	optimizeDeps: {
+		include: ['@carbon/charts', 'carbon-components-svelte'],
+	},
+	ssr: {
+		noExternal: ['@carbon/charts', 'carbon-components']
+	},
 };
 
 export default config;
